d92b59c60e3aae2430222fb52b4ebb7844dd8062
Abhijit Parida <abhijitparida.me@gmail.com>
2020-03-31 16:02:00 +0530

    Recurring tasks go in calender now

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const day = days[today.getDay()];
 const date = `${today.getDate()} ${months[today.getMonth()]}`;
-const underline = string => `<u>${string}</u>`;
-const title = `${today.getDay() === 0 ? underline(day) : day}, ${today.getDate() === 1 ? underline(date) : date}`;
-document.getElementById('title').innerHTML = title;
+document.getElementById('title').innerHTML = `${day}, ${date}`;
 
 const urls = {
   keep: 'https://keep.google.com',
@@ -14,7 +12,6 @@ const urls = {
   nextActions: 'https://keep.google.com/#LIST/1RAfNNBFWYn74BtBl_ze2LFV8L8E9Vq5JManSdY4g3-HJYfCXPqMLs2yrt_Hy-g',
   waitingFor: 'https://docs.google.com/document/d/1jOnInJ2Ca0izeNc2_qW-cZzW_9og5oYe8EGbgyz6tKA/edit',
   projects: 'https://docs.google.com/document/d/1iGmdvWvtVJiofrLGQObrM6QhK6OzFSFCfuP8tuUFrCk/edit',
-  recurring: 'https://docs.google.com/document/d/1wafeFixdDoUePFrv-OmFeo7ChgAtHZRhUiFCqL0jekU/edit',
   someday: 'https://docs.google.com/document/d/1dFZVGZ34LEU-84KS8-_HRA9BAST9sqUHlos9yYQKZLI/edit'
 };
 document.querySelectorAll('button').forEach(button => {
