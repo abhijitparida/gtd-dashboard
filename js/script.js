3ce9f65f1a16de0f9a5cf1fbbf7977aeff8b9f86
Abhijit Parida <abhijitparida.me@gmail.com>
2020-06-02 13:10:40 +0530

    waiting for -> blockers

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const urls = {
   mail: 'https://mail.google.com',
   calendar: 'https://calendar.google.com',
   nextActions: 'https://keep.google.com/#LIST/1RAfNNBFWYn74BtBl_ze2LFV8L8E9Vq5JManSdY4g3-HJYfCXPqMLs2yrt_Hy-g',
-  waitingFor: 'https://docs.google.com/document/d/1jOnInJ2Ca0izeNc2_qW-cZzW_9og5oYe8EGbgyz6tKA/edit',
+  blockers: 'https://docs.google.com/document/d/1jOnInJ2Ca0izeNc2_qW-cZzW_9og5oYe8EGbgyz6tKA/edit',
   projects: 'https://docs.google.com/document/d/1iGmdvWvtVJiofrLGQObrM6QhK6OzFSFCfuP8tuUFrCk/edit',
   someday: 'https://docs.google.com/document/d/1dFZVGZ34LEU-84KS8-_HRA9BAST9sqUHlos9yYQKZLI/edit'
 };
